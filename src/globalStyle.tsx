597fe1d2c48cb921a235178c08a0b15f34066d12
LucasAlAlam <lucasaars@dcc.ufrj.br>
2023-08-13 05:35:32 +0000

    config: definição de cores usadas

---
+++ src/globalStyle.tsx
@@ -0,0 +1,12 @@
+export const global = {
+    colors: {
+        azulBackground: '#0A0D24',
+        azulComponent: '#070611',
+        azulButton: '#020B61',
+        branco: '#ffffff',
+        preto: '#000000',
+        vermelhoSenhaFraca: '#FF0000',
+        azulSenhaRegular: '#00BFFF',
+        verdeSenhaForte: '#00FF00'
+    },
+};
